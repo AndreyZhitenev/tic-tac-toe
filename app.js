dd49e16107762f7a3454b0bce3de1ef7560505e1
AndreyZhitenev <zhitenev.andr@gmail.com>
2022-07-24 07:53:23 +0300

    determine winner

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const huPlayer = `<svg class="circle" width="24" height="24" viewBox="-3 -3 30 3
 <path fill-rule="evenodd" clip-rule="evenodd" d="M24 12C24 18.6274 18.6274 24 12 24C5.37258 24 0 18.6274 0 12C0 5.37258 5.37258 0 12 0C18.6274 0 24 5.37258 24 12ZM12 19.0926C15.9171 19.0926 19.0926 15.9171 19.0926 12C19.0926 8.08287 15.9171 4.90741 12 4.90741C8.08287 4.90741 4.90741 8.08287 4.90741 12C4.90741 15.9171 8.08287 19.0926 12 19.0926Z" fill="white"/>
 </svg>`,
 	aiPlayer = `
-	<svg class="cross "fill="#fff" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="30px" height="30px">    <path d="M 7 4 C 6.744125 4 6.4879687 4.0974687 6.2929688 4.2929688 L 4.2929688 6.2929688 C 3.9019687 6.6839688 3.9019687 7.3170313 4.2929688 7.7070312 L 11.585938 15 L 4.2929688 22.292969 C 3.9019687 22.683969 3.9019687 23.317031 4.2929688 23.707031 L 6.2929688 25.707031 C 6.6839688 26.098031 7.3170313 26.098031 7.7070312 25.707031 L 15 18.414062 L 22.292969 25.707031 C 22.682969 26.098031 23.317031 26.098031 23.707031 25.707031 L 25.707031 23.707031 C 26.098031 23.316031 26.098031 22.682969 25.707031 22.292969 L 18.414062 15 L 25.707031 7.7070312 C 26.098031 7.3170312 26.098031 6.6829688 25.707031 6.2929688 L 23.707031 4.2929688 C 23.316031 3.9019687 22.682969 3.9019687 22.292969 4.2929688 L 15 11.585938 L 7.7070312 4.2929688 C 7.5115312 4.0974687 7.255875 4 7 4 z"/></svg>`;
+	<svg class="cross" fill="none" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="30px" height="30px">    <path d="M 7 4 C 6.744125 4 6.4879687 4.0974687 6.2929688 4.2929688 L 4.2929688 6.2929688 C 3.9019687 6.6839688 3.9019687 7.3170313 4.2929688 7.7070312 L 11.585938 15 L 4.2929688 22.292969 C 3.9019687 22.683969 3.9019687 23.317031 4.2929688 23.707031 L 6.2929688 25.707031 C 6.6839688 26.098031 7.3170313 26.098031 7.7070312 25.707031 L 15 18.414062 L 22.292969 25.707031 C 22.682969 26.098031 23.317031 26.098031 23.707031 25.707031 L 25.707031 23.707031 C 26.098031 23.316031 26.098031 22.682969 25.707031 22.292969 L 18.414062 15 L 25.707031 7.7070312 C 26.098031 7.3170312 26.098031 6.6829688 25.707031 6.2929688 L 23.707031 4.2929688 C 23.316031 3.9019687 22.682969 3.9019687 22.292969 4.2929688 L 15 11.585938 L 7.7070312 4.2929688 C 7.5115312 4.0974687 7.255875 4 7 4 z" fill="#fff"/></svg>`;
 
 const winCombos = [
 	[0, 1, 2],
@@ -40,4 +40,28 @@ function turnClick(square) {
 function turn(squareId, player) {
 	origBoard[squareId] = player;
 	document.getElementById(squareId).innerHTML = player;
+	let gameWon = checkWin(origBoard, player);
+	if (gameWon) gameOver(gameWon);
+}
+
+function checkWin(board, player) {
+	let plays = board.reduce((a, e, i) => (e === player ? a.concat(i) : a), []);
+	let gameWon = null;
+	for (let [index, win] of winCombos.entries()) {
+		if (win.every((elem) => plays.indexOf(elem) > -1)) {
+			gameWon = { index, player };
+			break;
+		}
+	}
+	return gameWon;
+}
+
+function gameOver(gameWon) {
+	for (let index of winCombos[gameWon.index]) {
+		document.getElementById(index).querySelector("path").style.fill =
+			gameWon.player == huPlayer ? "hsla(211.27deg, 100%, 50%, 0.9)" : "white";
+	}
+	for (let i = 0; i < cells.length; i++) {
+		cells[i].removeEventListener("click", turnClick, false);
+	}
 }
